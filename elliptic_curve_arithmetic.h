f1235e19ebf0006cb5e0cea024605fff3d338020
Santiago León Ortiz <santileortiz@gmail.com>
2015-11-27 23:13:21 -0600

    Agregando función de doblado de puntos

--- elliptic_curve_arithmetic.h
+++ elliptic_curve_arithmetic.h
@@ -8,5 +8,6 @@ typedef struct _E_Fp_point {
 
 void elliptic_point_init (E_Fp_point P);
 void elliptic_point_add ( E_Fp_point P, E_Fp_point Q, E_Fp_point R);
+void elliptic_point_double (E_Fp_point P, E_Fp_point R);
 void elliptic_scalar_multiplication (uint64_t * k, E_Fp_point P, E_Fp_point R);
 
